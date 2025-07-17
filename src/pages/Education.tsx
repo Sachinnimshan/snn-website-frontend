e288a7c62c9116697547398768c16dae66260a69
Sachin Nimshan <sachinnimshan@gmail.com>
2025-07-17 21:28:24 +0530

    remove unused

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Education = () => {
-  return (
-    <div>Education</div>
-  )
-}
+  return <div>Education</div>;
+};
 
-export default Education
+export default Education;
