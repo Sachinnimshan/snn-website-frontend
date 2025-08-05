64ea3e9e64f9bcc54e21977a5866cb6d2d06c6d1
Sachin Nimshan <sachinnimshan@gmail.com>
2025-08-05 19:05:16 +0530

    remove  vite prerenderer

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
-  plugins: [react(), prerender({
-    staticDir: "dist",
-    routes: ["/", "/about", "/projects", "/experience", "/skills", "/academic", "/certifications", "/contact"],
-    postProcess(renderedRoute) {
-      return renderedRoute;
-    },
-  }),],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
