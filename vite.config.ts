4be002aa2e1b8ac4ff36b7f67398ec423153659e
Sachin Nimshan <sachinnimshan@gmail.com>
2025-08-05 18:50:08 +0530

    add vite prerenderer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), prerender({
+    staticDir: "dist",
+    routes: ["/", "/about", "/projects", "/experience", "/skills", "/academic", "/certifications", "/contact"],
+    postProcess(renderedRoute) {
+      return renderedRoute;
+    },
+  }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
