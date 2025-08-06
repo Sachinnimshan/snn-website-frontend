e0c7418b95ada821690225941514ccdf34d2e759
Sachin Nimshan <sachinnimshan@gmail.com>
2025-08-06 19:46:46 +0530

    install vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  Sitemap({
+    hostname: 'https://sachinnimshan.com',
+    // optional: dynamicRoutes: ['/path1', '/path2'],
+    // other options
+  }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
